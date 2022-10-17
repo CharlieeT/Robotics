ada62319e3c38557c95d787f282cd64532a50ccd
Tun Tun Lin <104047052+ttlinn123@users.noreply.github.com>
2022-10-17 12:54:59 +1100

    Update Collisiontestsample.m

--- Code/Simulation/Collisiontestsample.m
+++ Code/Simulation/Collisiontestsample.m
@@ -5,7 +5,7 @@ function isCollision = CheckCollision(robot, sphereCenter, radius)
     tr = robot.fkine(robot.getpos);
     endEffectorToCenterDist = sqrt(sum((sphereCenter-tr(1:3,4)').^2));
     if endEffectorToCenterDist <= radius
-%         disp('Oh no a collision!');
+%         disp('A collision detected');
         isCollision = 1;
     else
 %         disp(['SAFE: End effector to sphere centre distance (', num2str(endEffectorToCenterDist), 'm) is more than the sphere radius, ' num2str(radius), 'm']);
