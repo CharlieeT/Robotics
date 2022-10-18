041cb85e293e997f85555e0b929a55ec736d43d2
Tun Tun Lin <104047052+ttlinn123@users.noreply.github.com>
2022-10-18 11:10:30 +1100

    Update Dobot.m

--- Code/Simulation/@Dobot/Dobot.m
+++ Code/Simulation/@Dobot/Dobot.m
@@ -48,7 +48,7 @@ function GetDobot(self)
 end
 
 %% PlotAndColourRobot
-% Given a robot index, add the glyphs (vertices and faces) and
+% Given a robot index, adding the glyphs (vertices and faces) and
 % colour them in if data is available 
 
 function PlotAndColourRobot(self)%robot,workspace)
@@ -67,7 +67,7 @@ function PlotAndColourRobot(self)%robot,workspace)
     if isempty(findobj(get(gca,'Children'),'Type','Light'))
         camlight
     end  
-    self.model.delay = 0;
+    self.model.delay = 0.0;
 
     % Try to correctly colour the arm (if colours are in ply file data)
     for linkIndex = 0:self.model.n
