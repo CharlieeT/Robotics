cc407950a8b19e34e860f912dd3e5c81d76e59b9
Justin Sia <justin.sia@student.uts.edu.au>
2022-10-24 08:54:48 +1100

    Revert "Merge branch 'main' of https://github.com/CharlieeT/Robotics"
    This reverts commit 194c2e7dbbe554ea3c93e446bf3a6a6ea981765a, reversing
    changes made to 95c767dec8e94b078a1e454e9c29b12f5bf7b79e.

--- Code/Simulation/Collision.m
+++ Code/Simulation/Collision.m
@@ -19,54 +19,30 @@ classdef Collision < handle
             end
             
         end
-        %% Create Prism around robot for collision detection
+        %% Create Epllisoid around robot for collision detection
         function wallPoints = createWall()
-            [Y,Z] = meshgrid(-1.5:0.01:2,0.1:0.01:1.6);
+            [Y,Z] = meshgrid(-1:0.01:1,0.1:0.01:1.5);
             sizeMat = size(Y);
-            X = repmat(-2,sizeMat(1),sizeMat(2));
+            X = repmat(1,sizeMat(1),sizeMat(2));
             wallPoints = [X(:),Y(:),Z(:)];
             
-            %cubeAtOigin_h = plot3(wallPoints(:,1),wallPoints(:,2),wallPoints(:,3),'r.');
-        end
-        %% Create Prism around robot work area for collision detection
-        function wallPoints = createWall2()
-            [Y,Z] = meshgrid(-0.2:0.01:0.2,0.6:0.01:1);
-            sizeMat = size(Y);
-            X = repmat(0.2,sizeMat(1),sizeMat(2));
-            wallPoints = [X(:),Y(:),Z(:)];
-            x(1:1681,1) = -0.8;
-            y(1:1681,1) = 0;
-            z(1:1681,1) = -0.8;
-            z2(1:1681,1) = -0.4;
-            f = [x,y,z2];
-            g = [x,y,z];
-            wallPoints = [ wallPoints; wallPoints * rotz(pi/2); wallPoints * rotz(pi); wallPoints * rotz(3*pi/2);wallPoints*roty(pi/2)-f;wallPoints * roty(pi/2)-g;];
-            %cubeAtOigin_h = plot3(wallPoints(:,1),wallPoints(:,2),wallPoints(:,3),'r.');
+            wallPoints = [ wallPoints; wallPoints * rotz(pi/2)...
+                ; wallPoints * rotz(pi); wallPoints * rotz(3*pi/2)];
+            cubeAtOigin_h = plot3(wallPoints(:,1),wallPoints(:,2),wallPoints(:,3),'r.');
         end
         %% Check for collision
-        function isCollision(wallPoints, objectCenter, objectRadii, Obstacle)
-            id = 5;
-            joy = vrjoystick(id);
+        function isCollision(wallPoints, objectCenter, objectRadii)
             algebraicDist = GetAlgebraicDist(wallPoints, objectCenter, objectRadii);
             pointsInside = find(algebraicDist<=1);
             if (pointsInside > 0)
-                disp("Object Detected!");
-                while (pointsInside > 0)
-                    objectCenter = Obstacle.ObstaclePose(1:3,4);
-                    algebraicDist = GetAlgebraicDist(wallPoints, objectCenter, objectRadii);
-                    pointsInside = find(algebraicDist<=1);
-                    joystick1(Obstacle, joy);
-                end
-                input("Object has been removed, press enter to continue");
-            end
-            
-            function algebraicDist = GetAlgebraicDist(points, centerPoint, radii)
-            algebraicDist = ((points(:,1)-centerPoint(1))/radii(1)).^2 ...
-                + ((points(:,2)-centerPoint(2))/radii(2)).^2 ...
-                + ((points(:,3)-centerPoint(3))/radii(3)).^2;
+                disp('Object Detected');
             end
         end
         %% Get algebraic distance
-        
+        function algebraicDist = GetAlgebraicDist(points, centerPoint, radii)
+            algebraicDist = ((points(:,1)-centerPoint(1))/radii(1)).^2 ...
+                + ((points(:,2)-centerPoint(2))/radii(2)).^2 ...
+                + ((points(:,3)-centerPoint(3))/radii(3)).^2;
+        end
     end
 end
