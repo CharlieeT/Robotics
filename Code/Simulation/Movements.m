40da8013051d569e86b4e971412edf0e2ece2cee
JustSia <114462972+JustSia@users.noreply.github.com>
2022-10-23 13:52:17 +1100

    Added Joystick

--- Code/Simulation/Movements.m
+++ Code/Simulation/Movements.m
@@ -27,43 +27,6 @@ classdef Movements < handle
                 end
                 drawnow();
             end
-            function joystick1(Obstacle, joy)
-
-            [axes, buttons, povs] = read(joy);
-            if double(buttons(1)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)-0.01;
-                down = Obstacle.ObstaclePose;
-                Obstacle.move(down);
-                drawnow();
-            elseif double(buttons(2)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)-0.01;
-                right = Obstacle.ObstaclePose;
-                Obstacle.move(right);
-                drawnow();
-            elseif double(buttons(3)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)+0.01;
-                left = Obstacle.ObstaclePose;
-                Obstacle.move(left);
-                drawnow();
-            elseif double(buttons(4)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)+0.01;
-                up = Obstacle.ObstaclePose;
-                Obstacle.move(up);
-                drawnow();
-            elseif double(buttons(5)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)-0.01;
-                forward = Obstacle.ObstaclePose;
-                Obstacle.move(forward);
-                drawnow();
-            elseif double(buttons(6)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)+0.01;
-                backward = Obstacle.ObstaclePose;
-                Obstacle.move(backward);
-                drawnow();
-            end
-
-            pause(0.05);
-        end
         end
         %% Movement objects and robot using inverse kinematics
         function moveobji(robot,linkNum,T,obj,steps, Obstacle)
@@ -90,43 +53,6 @@ classdef Movements < handle
                 end
                 drawnow();
             end
-            function joystick1(Obstacle, joy)
-
-            [axes, buttons, povs] = read(joy);
-            if double(buttons(1)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)-0.01;
-                down = Obstacle.ObstaclePose;
-                Obstacle.move(down);
-                drawnow();
-            elseif double(buttons(2)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)-0.01;
-                right = Obstacle.ObstaclePose;
-                Obstacle.move(right);
-                drawnow();
-            elseif double(buttons(3)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)+0.01;
-                left = Obstacle.ObstaclePose;
-                Obstacle.move(left);
-                drawnow();
-            elseif double(buttons(4)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)+0.01;
-                up = Obstacle.ObstaclePose;
-                Obstacle.move(up);
-                drawnow();
-            elseif double(buttons(5)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)-0.01;
-                forward = Obstacle.ObstaclePose;
-                Obstacle.move(forward);
-                drawnow();
-            elseif double(buttons(6)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)+0.01;
-                backward = Obstacle.ObstaclePose;
-                Obstacle.move(backward);
-                drawnow();
-            end
-
-            pause(0.05);
-        end
         end
         %% Resolve Motion Rate Control
         function rmrc(robot,linkNum, finalPos, obj, steps, Obstacle)
@@ -180,42 +106,6 @@ classdef Movements < handle
                 end
                 drawnow();
             end
-            function joystick1(Obstacle, joy)
-            [axes, buttons, povs] = read(joy);
-            if double(buttons(1)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)-0.01;
-                down = Obstacle.ObstaclePose;
-                Obstacle.move(down);
-                drawnow();
-            elseif double(buttons(2)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)-0.01;
-                right = Obstacle.ObstaclePose;
-                Obstacle.move(right);
-                drawnow();
-            elseif double(buttons(3)) == 1
-                Obstacle.ObstaclePose(2,4) = Obstacle.ObstaclePose(2,4)+0.01;
-                left = Obstacle.ObstaclePose;
-                Obstacle.move(left);
-                drawnow();
-            elseif double(buttons(4)) == 1
-                Obstacle.ObstaclePose(3,4) = Obstacle.ObstaclePose(3,4)+0.01;
-                up = Obstacle.ObstaclePose;
-                Obstacle.move(up);
-                drawnow();
-            elseif double(buttons(5)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)-0.01;
-                forward = Obstacle.ObstaclePose;
-                Obstacle.move(forward);
-                drawnow();
-            elseif double(buttons(6)) == 1
-                Obstacle.ObstaclePose(1,4) = Obstacle.ObstaclePose(1,4)+0.01;
-                backward = Obstacle.ObstaclePose;
-                Obstacle.move(backward);
-                drawnow();
-            end
-
-            pause(0.05);
-        end
         end
         %% Movement using RMRC with obstacle
         function rmrcObj(robot,linkNum, finalPos, obj,objStart, objEnd, cond, steps)
